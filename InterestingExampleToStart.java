88be7ef3cdbac0b45e95c0e5801c3df7438e586a
Eason Liu <43985210+YusenLiu@users.noreply.github.com>
2020-03-21 10:44:19 -0500

    Create InterestingExampleToStart.java

---
+++ InterestingExampleToStart.java
@@ -0,0 +1,11 @@
+//This is an interesting example of the introduction to data structures
+//This is a method defined in the class Carrot
+
+
+public void waterCarrots(){
+  Carrot current = garden.findNextCarrot();
+  while(!waterCan.isEmpty()){
+    water(current, waterCan);
+    current = garden.findNextCarrot
+  }
+}
